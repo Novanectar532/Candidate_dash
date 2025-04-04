6c16a93a05f4a65962d91635fbb4d22ac58b01e6
gangwar22 <rahulgangwar22@navgurukul.org>
2025-04-05 00:10:18 +0530

    Add the files Login details , profile setting , profile pge , educationskillsform ,Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Applayout from './Applayout'
@@ -9,6 +6,10 @@ import Dashboard from './component/dashboard\'/Dashboard'
 import Findjob from './component/findjob/Findjob'
 import JobDetails from './component/findjob/JobDetails'
 import MyApplications from './component/application/MyApplications'
+import ProfilePage from './pages/ProfilePage'
+import ProfileSettings from './pages/ProfileSetting'
+import EducationSkillsForm from './pages/EducationSkillForm'
+import LoginDetails from './pages/LoginDetails'
 
 function App() {
   const router = createBrowserRouter([
@@ -27,11 +28,27 @@ function App() {
           element:<JobDetails/>
         },{
           path:'/myapplication',
-          element:<MyApplications/> }
+          element:<MyApplications/> },
         // },{
         //   path:'/message',
         //   element:<Mymessages/>
         // }
+        {
+          path:'/profilePage',
+          element:<ProfilePage/>
+        },
+        {
+          path:'/profilesetting',
+          element:<ProfileSettings/>
+        },
+        {
+          path:'/educationskillsform',
+          element:<EducationSkillsForm/>
+        },
+        {
+          path:'/logindetails',
+          element:<LoginDetails/>
+        }
       ]
     }
   ])
